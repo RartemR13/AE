31ad62cf0e0446ffc28b88a554e89b8f6b1357c1
RartemR13 <RartemR13@yandex.ru>
2023-06-07 00:26:51 +0300

    add some features for BytesArray

--- src/primitive/BytesArray.hpp
+++ src/primitive/BytesArray.hpp
@@ -1,11 +1,54 @@
 #pragma once
 
+#include <cstring>
+#include <initializer_list>
+#include <iostream>
+
+namespace ae {
+namespace primitive {
+
 template <uint8_t CountElements>
-  requires requires { CountElements > 0; }
+requires requires { CountElements > 0; }
 class BytesArray {
  public:
   BytesArray() = default;
 
+  ~BytesArray() = default;
+
+  BytesArray(const BytesArray<CountElements>& other) {
+    std::memcpy(storage_, &other, CountElements);
+  }
+
+  BytesArray(BytesArray<CountElements>&& other) = delete;
+
+  BytesArray<CountElements>& operator=(const BytesArray<CountElements>& other) {
+    if (this == &other) {
+      return *this;
+    }
+
+    std::memcpy(storage_, &other, CountElements);
+    return *this;
+  }
+
+  BytesArray<CountElements>& operator=(BytesArray<CountElements>&& other) =
+      delete;
+
+  BytesArray(std::initializer_list<uint8_t> initializer_list) {
+    if (initializer_list.size() != CountElements) {
+      throw std::invalid_argument(
+          "BytesArray, initilize_list.size() != CountElements.");
+    }
+
+    std::memcpy(&storage_, std::data(initializer_list), CountElements);
+  }
+
+  bool operator==(const BytesArray<CountElements>& other) {
+    return std::memcmp(&storage_, &other, CountElements) == 0;
+  }
+
  private:
   uint8_t storage_[CountElements]{};
 };
+
+}  // namespace primitive
+}  // namespace ae
--- tests/primitive/TestBytesArray.cpp
+++ tests/primitive/TestBytesArray.cpp
@@ -2,21 +2,53 @@
 
 #include "../../src/primitive/BytesArray.hpp"
 
-#include <cstring>
-#include <cstdlib>
-
 TEST(BytesArray, TestSize) {
-    BytesArray<6> foo;
+    ae::primitive::BytesArray<6> foo;
     EXPECT_EQ(sizeof(foo), 6);
 }
 
 TEST(BytesArray, TestStartStorage) {
-    BytesArray<10> foo;
+    ae::primitive::BytesArray<10> foo;
     
-    uint8_t bytes[10];
-    for (int i = 0; i < 10; ++i) {
-        bytes[i] = 0;
-    }
-    
+    uint8_t bytes[10] = {};
     ASSERT_TRUE(std::memcmp(&foo, bytes, 10) == 0);
+}
+
+TEST(BytesArray, TestCopyConstructor) {
+    ae::primitive::BytesArray<4> foo1;
+    
+    uint8_t bytes[4] = {1, 2, 3, 4};
+    std::memcpy(&foo1, bytes, 4);
+
+    ae::primitive::BytesArray<4> foo2(foo1);
+    ASSERT_TRUE(std::memcmp(&foo2, bytes, 4) == 0);
+}
+
+TEST(BytesArray, TestCopyOperator) {
+    ae::primitive::BytesArray<4> foo1;
+
+    uint8_t bytes[4] = {1, 2, 3, 4};
+    std::memcpy(&foo1, bytes, 4);
+
+    auto foo2 = foo1;
+    ASSERT_TRUE(std::memcmp(&foo2, bytes, 4) == 0);
+}
+
+TEST(BytesArray, TestInitilizerList) {
+    ae::primitive::BytesArray<4> foo = {1, 2, 3, 4};
+    
+    uint8_t bytes[4] = {1, 2, 3, 4};
+    ASSERT_TRUE(std::memcmp(&foo, bytes, 4) == 0);
+
+    auto thrower = [] {
+        ae::primitive::BytesArray<3> who_throw{1, 2, 3, 4};
+    };
+    ASSERT_THROW(thrower(), std::invalid_argument);
+}
+
+TEST(BytesArray, TestCompareOperator) {
+    auto foo1 = ae::primitive::BytesArray<4>{1, 2, 3, 4};
+    auto foo2 = foo1;
+
+    ASSERT_TRUE(foo1 == foo2);
 }
